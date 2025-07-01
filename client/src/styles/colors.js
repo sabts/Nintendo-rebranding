8df01e588eae66e404d057a98d2a2d62cc747b4d
sabts <sabrinataraschi@gmail.com>
2025-07-01 12:42:15 +0200

    .

---
+++ client/src/styles/colors.js
@@ -0,0 +1,35 @@
+const COLORS = {
+	base: {
+		primary: '#FBFBFB',
+		secondary: '#2F2F2F',
+		tertiary: '#E70012',
+		accent: '#FFFD5E'
+	},
+	variants: {
+		secondaryOpacity: 'rgba(47, 47, 47, 0.32)',
+		tertiarySaturated: '#B6000D',
+		primaryOpacity: '#E8E8E8'
+	},
+	gradients: {
+		tertiary:
+			'linear-gradient(180deg, rgba(231, 0, 18, 0.71) -15.25%, rgba(182, 0, 13, 0.71) 100%)',
+		accent:
+			'radial-gradient(74.07% 74.01% at 49.86% 88.56%, #FFFD5E 0%, #D7BC15 100%)'
+	},
+	userImageBackground: {
+		white: '#FFFFFF',
+		yellow: '#F3D50F',
+		orange: '#FF6203',
+		red: '#E70012',
+		lightPink: '#FFA1D7',
+		pink: '#F95496',
+		purple: '#6B44E0',
+		blue: '#0256A4',
+		lightBlue: '#00D2FF',
+		lightGreen: '#74AF47',
+		green: '#274E36',
+		black: '#2F2F2F'
+	}
+};
+
+export default COLORS;
