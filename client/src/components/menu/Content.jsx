b401a1fc8ea3cb64f6623b534ce0752c6aba056a
sabts <sabrinataraschi@gmail.com>
2025-07-02 19:05:54 +0200

    .

--- client/src/components/menu/Content.jsx
+++ client/src/components/menu/Content.jsx
@@ -1,32 +1,43 @@
 import { Link } from 'react-router-dom';
 import { StyledContentButtons, StyledContentContainer } from './menu-styles';
+import { useState } from 'react';
 
 const Content = ({ items }) => {
+  const [visibleSubitems, setVisibleSubitems] = useState(false);
   return (
     <StyledContentContainer>
-      {items.map((item, index) => {
-        if (item.subitems) {
-          return (
-            <div key={index}>
-              <div>{item.label}</div>
-              <ul>
-                {item.subitems.map(subitem => (
-                  <li key={subitem.key}>
-                    <Link to={subitem.url}>{subitem.label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        } else {
-          return (
-            <StyledContentButtons key={item.key}>
-              <Link to={item.url}>{item.label}</Link>
-            </StyledContentButtons>
-          );
-        }
-      })}
-    </StyledContentContainer>
+    {items.map((item, index) => (
+      <div key={index}>
+        <div
+          onClick={() =>
+            setVisibleSubitems((prevState) => ({
+              ...prevState,
+              [item.key]: !prevState[item.key], // Alterna la visibilidad de los subitems
+            }))
+          }
+        >
+          {/* Si el item tiene subitems, muestra la flecha hacia abajo o hacia arriba */}
+          <div>
+            {item.label}
+            {item.subitems && (
+              <span>{visibleSubitems[item.key] ? '↓' : '↑'}</span>
+            )}
+          </div>
+        </div>
+
+        {/* Mostrar los subitems solo si están visibles */}
+        {visibleSubitems[item.key] && item.subitems && (
+          <ul>
+            {item.subitems.map((subitem) => (
+              <li key={subitem.key}>
+                <Link to={subitem.url}>{subitem.label}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    ))}
+  </StyledContentContainer>
   );
 };
 
