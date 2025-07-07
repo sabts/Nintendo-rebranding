1f2b117ef0c7193cdccad5b1e11c991207c1c185
sabts <sabrinataraschi@gmail.com>
2025-07-08 00:26:32 +0200

    .

--- client/src/pages/news-detail/NewsDetail.jsx
+++ client/src/pages/news-detail/NewsDetail.jsx
@@ -1,11 +1,13 @@
-import { Link, Links, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { NEWS } from "../../constants/news";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import { PrimaryButton } from "../../components/ui/buttons/Buttons";
+import { StyledContentWrapper, StyledHeroImg } from "./news-detail-styles";
 
 const  NewsDetail = () => {
     const { id } = useParams();
+    console.log(id); 
     const newsItem = NEWS.find((news) => news.slug === id); 
 
     if (!newsItem) {
@@ -14,19 +16,17 @@ const  NewsDetail = () => {
     return (
         <>
         <Header/>
-            <img src={newsItem.heroImage} alt={newsItem.title} />
+            <StyledHeroImg src={newsItem.heroImage} alt={newsItem.title} />
+<StyledContentWrapper>
+    <div>
       <h2>{newsItem.title}</h2>
       <p>{newsItem.datePublished}</p>
       <p>{newsItem.body.summary}</p>
+      </div>
 
-      {/* Mostrar imágenes directamente */}
       {newsItem.body.images && newsItem.body.images[0] && (
-        <img src={newsItem.body.images[0]} alt="Image 1" />
+        <img src={newsItem.body.images[0]} />
       )}
-      {newsItem.body.images && newsItem.body.images[1] && (
-        <img src={newsItem.body.images[1]} alt="Image 2" />
-      )}
-
       <div>
         {newsItem.body.sections.map((section, index) => (
           <div key={index}>
@@ -34,10 +34,14 @@ const  NewsDetail = () => {
             <p>{section.content}</p>
           </div>
         ))}
+         {newsItem.body.images && newsItem.body.images[1] && (
+        <img src={newsItem.body.images[1]} alt="Image 2" />
+      )}
  <Link to={newsItem.cta.url}>
           <PrimaryButton>{newsItem.cta.text}</PrimaryButton>
         </Link>
       </div>
+      </StyledContentWrapper>
           <Footer/>
         </>
       );
---
+++ client/src/pages/news-detail/news-detail-styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledContentWrapper = styled.div`
+	display: flex;
+	align-self: center;
+	flex-direction: column;
+	padding-inline: 20px;
+`;
+
+const StyledHeroImg = styled.img`
+	width: 100%;
+	height: 305px;
+`;
+
+export { StyledContentWrapper, StyledHeroImg };
