80769f6f37b0ba681cee295937658098280a8651
sabts <sabrinataraschi@gmail.com>
2025-07-15 20:04:35 +0200

    register

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,42 +1,46 @@
 const { model, Schema } = require("mongoose");
 
-
 const userSchema = new Schema({
-    userName: String,
-    userNickname: String,
-    region: String,
-    email: String,
-    password: String,
-    birthday: String,
-    //TODO: relacio 1-N
-    favorite: [],
-    gamesOwned: [{
-        gameInfo: {
-            gameCode: {
-                type: String,
-                required: true
-            },
-            dateRegistered: {
-                type: Date,
-                default: Date.now
-            }
+  userName: String,
+  userNickname: String,
+  region: String,
+  email: String,
+  password: String,
+  birthday: String,
+  //TODO: relacio 1-N
+  favorite: [],
+  gamesOwned: [
+    {
+      gameInfo: {
+        gameCode: {
+          type: String,
+          required: true,
         },
-        gameAchievements: [{
-            type: String
-        }]
-    }],
-    friends: [{
-        friends: [],
-        reques: []
-    }],
-    points: Number,
-    profilePicture: {
-        img: String,
-        backgroundColor: String
+        dateRegistered: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+      gameAchievements: [
+        {
+          type: String,
+        },
+      ],
     },
-    shoppingCart: []
-})
-
+  ],
+  friends: [
+    {
+      friends: [],
+      reques: [],
+    },
+  ],
+  points: Number,
+  profilePicture: {
+    img: String,
+    backgroundColor: String,
+  },
+  shoppingCart: [],
+});
 
 // -userID
 //     - userName
@@ -52,6 +56,9 @@ const userSchema = new Schema({
 //     - profile picture{ img, color }
 // -shoppingCart[]
 
-
 // relacion 1 N mongo db
 // RELACION N - M entre usarios en mongo db
+
+const User = model("user", userSchema);
+
+module.exports = User;
