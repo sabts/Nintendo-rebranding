85c62394e01bc7e44b7ab41646b270c50498ea17
sabts <sabrinataraschi@gmail.com>
2025-07-26 11:13:27 +0200

    .

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -47,36 +47,6 @@ const Register = () => {
 
       {/* SECCION DE CUMPLEAÑOS */}
       <Birthday/>
-      {/* <div>
-        <picture>
-          <source
-            media='(min-width: 1024px)'
-            srcSet='/register/birthday-register-process-mobile.png'
-          />
-          <source
-            media='(min-width: 768px) and (max-width: 1023px)'
-            srcSet='/register/birthday-register-process-mobile.png'
-          />
-          <source
-            media='(min-width: 380px)'
-            srcSet='/register/birthday-register-process-mobile.png'
-          />
-          <img
-            src='/register/birthday-register-process-mobile.png'
-            alt='Peach and Daisy caring a present in birtday section in register'
-          />
-        </picture>
-        <h4>Welcome to Nintendo!</h4>
-        <span>we want to know more about you! So, can you tell us when is your birthday?</span>
-        <label htmlFor="birthday" />
-        <input
-          id="birthday"
-          name="birthday"
-          type="date"
-          {...register("birthday", { required: true })}
-        />
-      </div> */}
-
       {/* SECCION DE FORMULARIO */}
       <div>
         <label>Email</label>
